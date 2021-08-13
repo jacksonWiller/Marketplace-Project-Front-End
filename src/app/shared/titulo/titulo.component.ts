a3b54ad511e8586f45accb6ff3f15a46e3872756
Jackson Duarte <jacksonwillerduarte@gmail.com>
2021-08-13 12:57:06 -0300

    modificando grid e alterando scss

--- src/app/shared/titulo/titulo.component.ts
+++ src/app/shared/titulo/titulo.component.ts
@@ -6,10 +6,12 @@ import { Router } from '@angular/router';
   templateUrl: './titulo.component.html',
   styleUrls: ['./titulo.component.scss'],
 })
+
+
 export class TituloComponent implements OnInit {
   @Input() titulo: string;
-  @Input() iconClass = 'fa fa-user';
-  @Input() subtitulo = 'Desde 2021';
+  @Input() iconClass = 'bi bi-box-seam';
+  @Input() subtitulo = '';
   @Input() botaoListar = false;
 
   constructor(private router: Router) {}
